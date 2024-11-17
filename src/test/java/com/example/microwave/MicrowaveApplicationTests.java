ad9500faa7ac6e014994db2ae70f340b8ddaa679
Aisa-u <aisa.youldasheva@gmail.com>
2024-11-17 16:41:34 +0500

    init

---
+++ src/test/java/com/example/microwave/MicrowaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrowaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
